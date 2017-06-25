1c327efe90a9743fa83c15cecbc6453d0421b3c4
AnabelSalomone <anabel.salomone@gmail.com>
2017-06-25 14:39:03 +0200

    added list of philosophers in the app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,18 @@ import { Component } from '@angular/core';
 export class Philosopher {
   id: number;
   name: string;
+  subject: string[];
 }
 
 @Component({
   selector: 'my-app',
   template: `
   <h1>{{title}}</h1>
+  <h2>My Philosophers</h2>
+  <div class="philosophers" *ngFor="let philosopher of philosophers">
+    <h4>{{philosopher.name}}</h4>
+    <p *ngFor="let item of philosopher.subject">{{item}}</p>
+    </div>
   <h2>{{philosopher.name}} details!</h2>
   <div><label>id: </label>{{philosopher.id}}</div>
  <div> <label>name: </label>
@@ -16,13 +22,71 @@ export class Philosopher {
   `,
 })
 
-//ngModel === v-model(vue.js)
+/*ngModel === v-model(vue.js)
+ *ngFor: The (*) prefix to ngFor is a critical part of this syntax. 
+        It indicates that the <li> element and its children constitute 
+        a master template.
+        It is also === v-for="let item of items" (Vue.js)
+        */
 
 
 export class AppComponent {
   title = 'Tour of Philosophers';
   philosopher: Philosopher = {
     id: 1,
-    name: 'Plato'
+    name: 'Plato',
+    subject: ["Metaphysics", "Gnoseology", "Aesthetics", "Anthropology"]
+  };
+  philosophers = PHILOSOPHERS;
+};
+
+const PHILOSOPHERS: Philosopher[] = [
+  {
+    id: 2,
+    name: "Aristotle",
+    subject: ["Ethics", "Metaphysics", "Gnoseology", "Political philosophy", "Anthropology"]
+  },
+  {
+    id: 3,
+    name: "René Descartes",
+    subject: ["Metaphysics", "Gnoseology"]
+  },
+  {
+    id: 4,
+    name: "Immanuel Kant",
+    subject: ["Gnoseology", "Metaphysics", "Ethics", "Aesthetics"]
+  },
+  {
+    id: 5,
+    name: "Saint Augustin",
+    subject: ["Metaphysics"]
+  },
+  {
+    id: 6,
+    name: "David Hume",
+    subject: ["Gnoseology"]
+  },
+  {
+    id: 7,
+    name: "Thomas Hobbes",
+    subject: ["Political philosophy"]
+  },
+  {
+    id: 8,
+    name: "Edgar Morin",
+    subject: ["Ethics", "Anthropology", "Constructivist epistemology"]
+  },
+  {
+    id: 9,
+    name: "Averroes",
+    subject: ["Metaphysics"]
+  },
+  {
+    id: 10,
+    name: "Baruch Spinoza",
+    subject: ["Metaphysics", "Ethics"]
   }
-}
+]
+
+
+
